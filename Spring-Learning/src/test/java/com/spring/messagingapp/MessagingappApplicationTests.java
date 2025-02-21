a722e9209c40d523fc9e7f3a56b00ecd6b4d5a27
amansingh977 <amansinghrajawat09@gmail.com>
2025-02-21 16:05:18 +0530

    Aman Add. uc1

---
+++ Spring-Learning/src/test/java/com/spring/messagingapp/MessagingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.messagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
